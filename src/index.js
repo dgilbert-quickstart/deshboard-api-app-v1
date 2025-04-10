a404621ba6221e1bf1b648d0f9e6c450287299da
dgilbert-quickstart <dgilbert.quickstart@gmail.com>
2025-04-10 20:38:16 +0100

    update dashboard api v1

---
+++ src/index.js
@@ -0,0 +1,36 @@
+const express = require("express");  
+const cors = require("cors");  
+const app = express();  
+
+//cors allow access to same site or other localhost 
+app.use(cors()) 
+
+//localhost port range - 3000 - 9999
+const SERVER_PORT = 8080; 
+
+app.get("/", (req,res) => {
+    const _msg = "# Demo node express api 1.0.0";
+    console.log(_msg)
+    res.send(_msg)
+});
+
+app.get("/about", (req,res) => {
+    const _msg = "# Demo node express api 1.0.0 - about route";
+    console.log(_msg)
+    res.send(_msg)
+});
+
+app.get("/test1", (req,res) => {
+    const _msg = "# Demo node express api 1.0.0 - test1 route";
+    console.log(_msg)
+    res.send(_msg)
+});
+
+//-other - POST, DELETE, PUT
+
+//-start node exporess web server - ie: live server
+app.listen(SERVER_PORT, ()=>{
+    let _msg = "node express websever running at port: " + SERVER_PORT;
+    console.log(_msg);
+})
+
