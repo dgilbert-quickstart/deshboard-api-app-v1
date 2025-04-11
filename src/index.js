e6f10e612d0c5dce89fccb8bfc2b6039a184d5ad
dgilbert-quickstart <dgilbert.quickstart@gmail.com>
2025-04-11 23:40:04 +0100

    update dashboard api v01

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");  
 const app = express();  
 const bodyParser = require("body-parser");
+const { login, insert_user } = require("./dbutil");
 
 //- node middleware
 app.use(bodyParser.json());
@@ -32,26 +33,29 @@ app.get("/test1", (req,res) => {
 });
 
 app.get("/login/:username/:password", (req, res) => {
-
     const _username = req.params.username;
     const _password = req.params.password;
 
     let _msg = `# login route, username: ${_username}, password: ${_password}`;
     console.log(_msg);
 
-     let _data = {};
+    let _data = {};
 
-     //validate login in database 
+    login(_username, _password, (islogin) => {
+        _msg = "* login successful";
+        _data = { msg: _msg, login: true };
 
-     _msg = "* login successful";
-    _data = { msg: _msg, login: true };
+        if (!islogin) {
+            _msg = "* invalid username/password";
+            _data = { msg: _msg, login: false };
+        }
 
-    //return json result 
-    res.send(_data);
+        res.send(_data);
+    });
 });
 
-app.post("/register", (req, res) => {
 
+app.post("/register", (req, res) => {
     const _body = req.body;
 
     let _msg = `# register route, body: ${JSON.stringify(_body)}`;
@@ -62,13 +66,17 @@ app.post("/register", (req, res) => {
     const _username = _body.username;
     const _password = _body.password;
 
-    //insert user row 
+    insert_user(_username, _password, (isnewuser) => {
+        _msg = "* registration successful";
+        _return = { msg: _msg, register: true };
 
-    _msg = "* registration successful";
-    _return = { msg: _msg, register: true };
+        if (!isnewuser) {
+            _msg = "* invalid registration, username already exists.";
+            _return = { msg: _msg, register: false };
+        }
 
-     res.send(_return);
-
+        res.send(_return);
+    });
 });
 
 //-other - POST, DELETE, PUT
